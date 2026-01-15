5f19a6cb257ae6d8efbde5d9e03c31bcb36ac8e8
kb8864 <yt7s858864@gmail.com>
2026-01-15 19:30:25 +0900

    intern()メソッドについて

--- JavaSilver/sample/Main.java
+++ JavaSilver/sample/Main.java
@@ -3,14 +3,23 @@ package sample;
 public class Main {
 
     public static void main(String[] args) {
-               String a = "sample";
-               String b = "sample";
-               System.out.print(a == b);
-               System.out.print(", ");
-              System.out.println(a.equals(b));
-              //true, true
-              //a と b は、物理的に同じメモリ上のオブジェクトを指している
-              //だから、住所を比較する == でも true になる
+      String a = "abc";
+      String b = new String(a);
+
+      int count = 0;
+      //intern()は「コンスタントプールの中にある『本物の在庫』の場所を返すと言う意味
+//      つまり、intern() を通すと、元のオブジェクトがヒープにあろうがどこにあろうが、必ず「プールの住所」が返ってくる
+      if(a.intern() == "abc"){
+        count++;
+      }
+      if(b.intern() == "abc"){
+        count++;
+
+      }
+      if(a.intern() == b.intern()){
+        count++;
+      }
+      System.out.println(count);
 
     }
 }
