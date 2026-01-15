33109e8e8fcafa2b22237df66aa2de47fd0a7ec2
kb8864 <yt7s858864@gmail.com>
2026-01-15 18:29:38 +0900

    コンスタントプールについて

--- JavaSilver/sample/Main.java
+++ JavaSilver/sample/Main.java
@@ -1,14 +1,16 @@
 package sample;
 
 public class Main {
-  public static void main(String[] args) {
-    Object a = new Object();
-    Object b = null;
-//    これはfalse。理由はnull と比較したら必ず false を返さなければならない
-    System.out.println(a.equals(b));
 
-    //逆だったら？
-    //System.out.println(a.equals(b));
-    //これは無いもんに対して比較しようとしているので、NullPointerException発生
-  }
+    public static void main(String[] args) {
+               String a = "sample";
+               String b = "sample";
+               System.out.print(a == b);
+               System.out.print(", ");
+              System.out.println(a.equals(b));
+              //true, true
+              //a と b は、物理的に同じメモリ上のオブジェクトを指している
+              //だから、住所を比較する == でも true になる
+
+    }
 }
