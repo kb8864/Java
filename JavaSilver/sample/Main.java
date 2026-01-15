8ec7f789bb7b8162d26db102e8a90443cc9b7344
kb8864 <yt7s858864@gmail.com>
2026-01-15 19:54:56 +0900

    {} 省略時の罠

--- JavaSilver/sample/Main.java
+++ JavaSilver/sample/Main.java
@@ -3,23 +3,8 @@ package sample;
 public class Main {
 
     public static void main(String[] args) {
-      String a = "abc";
-      String b = new String(a);
-
-      int count = 0;
-      //intern()は「コンスタントプールの中にある『本物の在庫』の場所を返すと言う意味
-//      つまり、intern() を通すと、元のオブジェクトがヒープにあろうがどこにあろうが、必ず「プールの住所」が返ってくる
-      if(a.intern() == "abc"){
-        count++;
-      }
-      if(b.intern() == "abc"){
-        count++;
-
-      }
-      if(a.intern() == b.intern()){
-        count++;
-      }
-      System.out.println(count);
-
+      if(false)
+        System.out.println("A");
+      System.out.println("B");
     }
 }
