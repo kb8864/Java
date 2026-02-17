13c98f1014e221bc34404cbf86196ab3662d9874
kb8864 <yt7s858864@gmail.com>
2026-02-17 19:39:05 +0900

    文法ミスした内容

---
+++ 01-01/src/example/Q3.java
@@ -0,0 +1,35 @@
+package example;
+
+import java.util.Scanner;
+
+public class Q3 {
+  //	長さ N の数列Aが与えられます。Aの中に 0 が含まれていない場合はYESを、 0 が含まれている場合はNOを出力してください。
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+//		まず数列の長さ N を受け取る
+    int n = sc.nextInt();
+//    長さ N の整数配列を準備する
+    int[] a = new int[n];
+
+    boolean hasZero = false;
+
+    for (i = 0; i < n ; i++){
+      a[i] = sc.nextInt();
+
+      if(a[i] == 0){
+        boolean hasZero = true;
+        System.out.println("YES");
+      }else(a[i] != 0){
+        boolean hasZero = false;
+        System.out.println("NO");
+      }
+
+
+    }
+
+    sc.close();
+
+  }
+}
